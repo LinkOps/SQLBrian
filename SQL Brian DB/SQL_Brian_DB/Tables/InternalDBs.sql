16c671da370b3a175fb27c9716546d7d9ba32ac1
Marcus Culver <MarcusCulver85@outlook.com>
2017-05-15 22:27:59 +0100

    Started to add Internal DB Lookup table for Implcit conversion reporting

---
+++ SQL Brian DB/SQL_Brian_DB/Tables/InternalDBs.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[InternalDBs]
+(
+	[ServerId] INT NOT NULL, 
+    [DatabaseId] INT NOT NULL,
+	CONSTRAINT [PK_InternalDBs] PRIMARY KEY ([ServerId],[DatabaseId])
+)
+GO
+
+ALTER TABLE [dbo].[InternalDBs]
+	ADD CONSTRAINT fk_InternalBds_Database
+	FOREIGN KEY ([ServerId], [DatabaseId])
+	REFERENCES [Database] ([ServerId], [DatabaseId])
